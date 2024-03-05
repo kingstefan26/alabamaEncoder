e40b276d34fe2d388a2aa53ba216e2de5dbd149c
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2024-03-05 21:46:37 +0100

    allow for multiple temp dirs in the same root dir, by adding a hash suffix thats derived from title
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- alabamaEncode_frontends/cli/cli_setup/paths.py
+++ alabamaEncode_frontends/cli/cli_setup/paths.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 
@@ -11,7 +12,10 @@ def parse_paths(ctx):
     # turn tempfolder into a full path
     ctx.output_folder = os.path.normpath(os.path.dirname(ctx.output_file) + "/")
 
-    ctx.temp_folder = os.path.join(ctx.output_folder, ".alabamatemp")
+    # derive random part from the first few letters of the sha1 hash of title
+    random_part = hashlib.sha1(ctx.get_title().encode("utf-8")).hexdigest()[:5]
+
+    ctx.temp_folder = os.path.join(ctx.output_folder, f".alabamatemp-{random_part}")
     if not os.path.exists(ctx.temp_folder):
         os.makedirs(ctx.temp_folder)
 
