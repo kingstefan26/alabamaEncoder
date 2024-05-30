7a2733707a1a1bdbf744ebd9f38e6880129c5b3c
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2024-05-30 04:21:17 +0200

    move get_yuv_frame_stream into the Ffmpeg file
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- alabamaEncode/core/extras/auto_thumbnailer.py
+++ alabamaEncode/core/extras/auto_thumbnailer.py
@@ -6,11 +6,10 @@ import scipy
 from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 
+from alabamaEncode.core.ffmpeg import Ffmpeg, get_yuv_frame_stream
 from alabamaEncode.core.util.bin_utils import get_binary, check_ffmpeg_libraries, check_bin
 from alabamaEncode.core.util.cli_executor import run_cli
-from alabamaEncode.core.ffmpeg import Ffmpeg
 from alabamaEncode.core.util.path import PathAlabama
-from alabamaEncode.ffmpeg_source.ffmpeg_src import get_yuv_frame_stream
 from alabamaEncode.scene.chunk import ChunkObject
 
 
