dc13f336dcb85d21f53eacfa9591680341023258
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2024-07-16 02:24:50 +0200

    make ffmpeg video custom filters work with scaling
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- alabamaEncode/cli/cli_setup/video_filters.py
+++ alabamaEncode/cli/cli_setup/video_filters.py
@@ -6,31 +6,20 @@ def parse_video_filters(ctx):
     """
     Sets up the video filters
     """
-    # make --video_filters mutually exclusive with --hdr --crop_string --scale_string
-    # if ctx.prototype_encoder.video_filters != "" and (
-    #     ctx.prototype_encoder.hdr
-    #     or ctx.prototype_encoder.video_filters != ""
-    #     or ctx.scale_string != ""
-    # ):
-    #     print(
-    #         "--video_filters is mutually exclusive with --hdr, --crop_string, and --scale_string"
-    #     )
-    #     quit()
 
-    if ctx.prototype_encoder.video_filters == "":
-        vec = []
+    vec = ctx.prototype_encoder.video_filters.split(",")
 
-        if ctx.crop_string != "":
-            vec.append(f"crop={ctx.crop_string}")
+    if ctx.crop_string != "":
+        vec.append(f"crop={ctx.crop_string}")
 
-        if ctx.scale_string != "":
-            vec.append(f"scale={ctx.scale_string}:flags=lanczos")
+    if ctx.scale_string != "":
+        vec.append(f"scale={ctx.scale_string}:flags=lanczos")
 
-        if ctx.prototype_encoder.hdr is False and Ffmpeg.is_hdr(
-            PathAlabama(ctx.input_file)
-        ):
-            print("Input Video is HDR but requested a SDR encode, auto-tonemapping")
-            vec.append(Ffmpeg.get_tonemap_vf())
+    if ctx.prototype_encoder.hdr is False and Ffmpeg.is_hdr(
+        PathAlabama(ctx.input_file)
+    ):
+        print("Input Video is HDR but requested a SDR encode, auto-tonemapping")
+        vec.append(Ffmpeg.get_tonemap_vf())
 
-        ctx.prototype_encoder.video_filters = ",".join(vec)
+    ctx.prototype_encoder.video_filters = ",".join(vec)
     return ctx
