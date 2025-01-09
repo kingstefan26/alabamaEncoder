84dc3b5698f86e89c60ab72fb7aa71a31170aa08
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2025-01-09 15:51:31 +0100

    ver bump
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alabamaEncoder",
-    version="0.5.3",
+    version="0.5.4",
     packages=find_packages(),
     install_requires=[
         "scenedetect",
