0c833581a573c363f31140b25efdf8be9dda351f
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2025-01-27 15:57:30 +0100

    save frame scores for xpsnr as well as vmaf in the frame_scores bucket
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- alabamaEncode/conent_analysis/chunk/final_encode_steps/plain.py
+++ alabamaEncode/conent_analysis/chunk/final_encode_steps/plain.py
@@ -4,7 +4,10 @@ from alabamaEncode.conent_analysis.chunk.final_encode_step import (
 from alabamaEncode.core.context import AlabamaContext
 from alabamaEncode.encoder.encoder import Encoder
 from alabamaEncode.encoder.stats import EncodeStats
+from alabamaEncode.metrics.comparison_display import ComparisonDisplayResolution
+from alabamaEncode.metrics.impl.xpsnr import XpsnrOptions
 from alabamaEncode.metrics.metric import Metric
+from alabamaEncode.metrics.options import MetricOptions
 from alabamaEncode.scene.chunk import ChunkObject
 
 
@@ -13,33 +16,43 @@ class PlainFinalEncode(FinalEncodeStep):
         self, enc: Encoder, chunk: ChunkObject, ctx: AlabamaContext, encoded_a_frame
     ) -> EncodeStats:
         metric, _ = ctx.get_metric_target()
+
+        metric_options = MetricOptions()
+
+        if metric == Metric.VMAF:
+            metric_options = ctx.get_vmaf_options()
+        elif metric == Metric.XPSNR:
+            metric_options = XpsnrOptions(
+                ref=(
+                    ComparisonDisplayResolution.from_string(ctx.vmaf_reference_display)
+                    if ctx.vmaf_reference_display
+                    else None
+                ),
+                denoise_refrence=ctx.denoise_vmaf_ref,
+                subsample=ctx.vmaf_subsample,
+            )
+
         stats = enc.run(
             metric_to_calculate=metric if ctx.calc_final_vmaf else None,
-            metric_params=ctx.get_vmaf_options(),
+            metric_params=metric_options,
             on_frame_encoded=encoded_a_frame,
         )
-        if metric == Metric.VMAF and ctx.calc_final_vmaf:
-            if stats.metric_results.frames is not None:
-                vmaf_frame_scores = {}
-                for frame in stats.metric_results.frames:
-                    vmaf_frame_scores[frame["frameNum"]] = frame["metrics"]["vmaf"]
-                ctx.get_kv().set(
-                    "vmaf_frame_scores",
-                    chunk.chunk_index,
-                    vmaf_frame_scores,
-                    individual_mode=True,
-                )
-        if metric == Metric.XPSNR and ctx.calc_final_vmaf:
-            if stats.metric_results.frames is not None:
-                frame_scores = {}
-                for frame in stats.metric_results.frames:
+
+        if ctx.calc_final_vmaf and stats.metric_results.frames is not None:
+            frame_scores = {}
+
+            for frame in stats.metric_results.frames:
+                if metric == Metric.VMAF:
+                    frame_scores[frame["frameNum"]] = frame["metrics"]["vmaf"]
+                if metric == Metric.XPSNR:
                     frame_scores[frame["frame"]] = frame["avg"]
-                ctx.get_kv().set(
-                    "frame_scores",
-                    chunk.chunk_index,
-                    frame_scores,
-                    individual_mode=True,
-                )
+
+            ctx.get_kv().set(
+                "frame_scores",
+                chunk.chunk_index,
+                frame_scores,
+                individual_mode=True,
+            )
 
         return stats
 
